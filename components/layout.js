b1c3c5a69f981c0d49f9ec0d9d8d56f755c1fe0b
kanno-takumi <bp20065@shibaura-it.ac.jp>
2023-01-23 03:10:39 +0900

    ブログの作成

---
+++ components/layout.js
@@ -0,0 +1,90 @@
+import styles from './layout.module.css';
+import Head from 'next/head';
+import Image from 'next/image';
+// import styles from '../styles/utils.module.css'
+import Link from 'next/link';
+import utilStyles from '../styles/utils.module.css'
+
+
+const name='Takumi Kanno';
+export const siteTitle='Next.js Sample Webite'
+export default function Layout({children,home}){
+    return (
+        <div className={styles.container}>{/**layout.module.css */}
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta 
+                     name="description"
+                     content="Learn how to build a personal website using Next.js"
+                />
+                <meta
+                    property="og:image"
+                    content={'https://og-image.vercel.app/${encodeURI(siteTitle,)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'}
+                    />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" /> 
+            </Head>
+            <header className={styles.header}>{/**layout.module.css */}
+                {home?(//条件
+                    <>
+                     {/* home=true→ルートディレクトリ(index)の時 */}
+                    <Image
+                    priority
+                    src="/images/profile.jpg"
+                    className={utilStyles.borderCircle}
+                    height={144}
+                    width={144}
+                    alt=""
+                    />
+                    <h1 className={utilStyles.heading2Xl}>{name}</h1>{/**utils.module.css */}
+                    <Link href="/posts/first-post">Sample Page</Link>
+                    </>
+                ):(//条件不一致の場合(post)
+                    <>
+                    <Link href="/">
+                        <Image
+                        priority
+                        src="/images/profile.jpg"
+                        className={utilStyles.borderCircle}
+                        height={108}
+                        width={108}
+                        alt=""
+                        />
+                    </Link>
+                    
+    {/*     styles/utils.module.css
+    .headingLg{
+    font-size:1.5rem;
+    line-height:1.4;
+    margin:1rem 0 ; 
+    */}
+     {/* .colorInherit {
+    color: inherit;
+    /* 親要素を引き継ぐ 
+    */}
+  
+
+                    <h2 className={utilStyles.headingLg}>{/**utils.module.css */}
+                        <Link href="/" className={utilStyles.colorInherit}>
+                        {/* <Link href="/"> */}
+                             {name}
+                        </Link>
+                    </h2>
+                    </>
+                )}
+                </header>
+                <main>{children}</main>
+                {/* {console.log({home})}
+                {console.log(!{home} +"確認用")} */}
+
+                {!home &&
+                (//ルートディレクトリにいない場合(index.jsでtrueを渡しているから)
+                    <div className={styles.backToHome}>{/**layout.module.css */}
+                        <Link href="/">Back to home</Link>
+                    </div> 
+                    
+                )}
+                {/* <Link href="/"><h2>stylesを使わないバージョン</h2></Link> */}
+        </div>
+    );
+}
---
+++ pages/index.js
@@ -0,0 +1,163 @@
+import Head from 'next/head'
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Date from '../components/date'
+import Layout,{siteTitle} from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps(){//getStaticPropsはpageからのみエクスポートされる
+  const allPostsData=getSortedPostsData();
+  return{
+    props:{//propsの中でalllpostsDataを返すことでHomeコンポーネントにpropとして渡す
+       allPostsData,
+    }
+  }
+}
+
+export default function Home({allPostsData}) {//allPostsDataを使える状態になった　
+  return (
+    // <div className={styles.container}>
+    //   <Head>
+    //     <title>Create Next App</title>
+    //     <link rel="icon" href="/favicon.ico" />
+    //   </Head>
+
+    //   <main>
+    //     <h1 className={styles.title}>
+    //       Learn <Link href="https://nextjs.org">Next.js!</Link>
+    //     </h1>
+
+    //     <p className={styles.description}>
+    //       Get started by editing <code>pages/index.js</code>
+    //     </p>
+
+    //     <div className={styles.grid}>
+    //       <a href="https://nextjs.org/docs" className={styles.card}>
+    //         <h3>Documentation &rarr;</h3>
+    //         <p>Find in-depth information about Next.js features and API.</p>
+    //       </a>
+
+    //       <a href="https://nextjs.org/learn" className={styles.card}>
+    //         <h3>Learn &rarr;</h3>
+    //         <p>Learn about Next.js in an interactive course with quizzes!</p>
+    //       </a>
+
+    //       <a
+    //         href="https://github.com/vercel/next.js/tree/master/examples"
+    //         className={styles.card}
+    //       >
+    //         <h3>Examples &rarr;</h3>
+    //         <p>Discover and deploy boilerplate example Next.js projects.</p>
+    //       </a>
+
+    //       <a
+    //         href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    //         className={styles.card}
+    //       >
+    //         <h3>Deploy &rarr;</h3>
+    //         <p>
+    //           Instantly deploy your Next.js site to a public URL with Vercel.
+    //         </p>
+    //       </a>
+    //     </div>
+    //   </main>
+
+    //   <footer>
+    //     <a
+    //       href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Powered by{' '}
+    //       <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+    //     </a>
+    //   </footer>
+
+    //   <style jsx>{`
+    //     main {
+    //       padding: 5rem 0;
+    //       flex: 1;
+    //       display: flex;
+    //       flex-direction: column;
+    //       justify-content: center;
+    //       align-items: center;
+    //     }
+    //     footer {
+    //       width: 100%;
+    //       height: 100px;
+    //       border-top: 1px solid #eaeaea;
+    //       display: flex;
+    //       justify-content: center;
+    //       align-items: center;
+    //     }
+    //     footer img {
+    //       margin-left: 0.5rem;
+    //     }
+    //     footer a {
+    //       display: flex;
+    //       justify-content: center;
+    //       align-items: center;
+    //       text-decoration: none;
+    //       color: inherit;
+    //     }
+    //     code {
+    //       background: #fafafa;
+    //       border-radius: 5px;
+    //       padding: 0.75rem;
+    //       font-size: 1.1rem;
+    //       font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+    //         DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+    //     }
+    //   `}</style>
+
+    //   <style jsx global>{`
+    //     html,
+    //     body {
+    //       padding: 0;
+    //       margin: 0;
+    //       font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    //         Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+    //         sans-serif;
+    //     }
+    //     * {
+    //       box-sizing: border-box;
+    //     }
+    //   `}</style>
+    // </div>
+
+    <Layout home={true}>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>{/**utils.module.css　$を使って複数classNameを追加できる */}
+      <h2 className={utilStyles.headiingLg}>Blog</h2>
+      <ul className={utilStyles.list}>{/**ul 箇条書き */}
+      {console.log(allPostsData)}
+      
+      {allPostsData.map(({ content, date, title, id }) => (
+        <li className={utilStyles.listItem} key={id} >
+          <Link href={`/posts/${id}`}>{title}</Link>
+          {/* <br />
+          {title}
+          <br />
+          {content}
+          <br />
+           */}
+            <small className={utilStyles.lightText}>{/*smallタグ→テキストを一回り小さくするタグ */}
+            <br />
+            <Date dateString={date} />
+            </small>
+            {console.log({date})}
+        </li>
+      ))}
+      </ul>
+        {/* <p>
+          大学3年生で、プログラミングについて学習中です。
+        </p> */}
+        {/* <p>
+          <a href="http://www.ktkr2020.com/myapps/portfolio/kanno-takumi.jsp">過去のポートフォリオはこちら</a>
+        </p> */}
+      </section>
+    </Layout>
+  )
+}
