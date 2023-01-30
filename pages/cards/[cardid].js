622a64ab2ad39f6abd56e23e2716c27f4dcb5fe5
kanno-takumi <bp20065@shibaura-it.ac.jp>
2023-01-31 08:08:17 +0900

    パスの設定

--- pages/cards/[cardid].js
+++ pages/cards/[cardid].js
@@ -1,50 +1,93 @@
-import Layout from '../../components/layout';
-import {getAllPostIds,getPostData} from '../../lib/cards';
-import Head from 'next/head';
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/cards'
+import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 
-
-//getStaticProps内でgetPostDataを使う
-//それをpropsとしてreturnする
-export async function getStaticProps({params}){//getStaticPathsによって指定された[id]からデータをfetchする
-    const postData=getPostData(params.cardid);
-    return {
-        props:{
-            postData,
-        }
-    }
-}
-
-export async function getStaticPaths(){//"pre-rendering"や"ssg-ssr"になるようにpathを設定している
-    const paths=getAllPostIds();
-    return{
-        paths,
-        fallback:false,
-    }
-}
-
-//posts/...の時に表示される部分
-export default function Post({postData}){//propsとしてpostDataを使える
-    return (
+export default function Post({ postData }) {
+  return (
     <Layout>
-        <Head>{/*ブラウザの上のタグ */}
-            <title>{postData.title}</title>
-        </Head>
-        <article>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <br />
-        <h2 className={utilStyles.headingMd}>{postData.content}</h2>
-        {/* <br />
-        {postData.id} */}
-        <br />
-        {/* {postData.date} */}
-        <br />
         <div className={utilStyles.lightText}>
-        <Date dateString={postData.date} />
+          <Date dateString={postData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
+        {/* <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> */}
+        <h2 className={utilStyles.headingMd}>{postData.content}</h2>
+      </article>
     </Layout>
-    )
+  )
 }
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.cardid)//ここも変更
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+
+
+// import Layout from '../../components/layout';
+// import {getAllPostIds,getPostData} from '../../lib/cards';
+// import Head from 'next/head';
+// import Date from '../../components/date'
+// import utilStyles from '../../styles/utils.module.css'
+
+
+// //getStaticProps内でgetPostDataを使う
+// //それをpropsとしてreturnする
+// export async function getStaticProps({params}){//getStaticPathsによって指定された[id]からデータをfetchする
+//     const postData=getPostData(params.cardid);
+//     return {
+//         props:{
+//             postData,
+//         }
+//     }
+// }
+
+// export async function getStaticPaths(){//"pre-rendering"や"ssg-ssr"になるようにpathを設定している
+//     const paths=getAllPostIds();
+//     return{
+//         paths,
+//         fallback:false,
+//     }
+// }
+
+// //posts/...の時に表示される部分
+// export default function Post({postData}){//propsとしてpostDataを使える
+//     return (
+//     <Layout>
+//         <Head>{/*ブラウザの上のタグ */}
+//             <title>{postData.title}</title>
+//         </Head>
+//         <article>
+//         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+//         <br />
+//         <h2 className={utilStyles.headingMd}>{postData.content}</h2>
+//         {/* <br />
+//         {postData.id} */}
+//         <br />
+//         {/* {postData.date} */}
+//         <br />
+//         <div className={utilStyles.lightText}>
+//         <Date dateString={postData.date} />
+//         </div>
+//         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+//         </article>
+//     </Layout>
+//     )
+// }
