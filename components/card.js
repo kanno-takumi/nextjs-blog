ad0a78c6d099cd9ce64b69a6ae4918e54da5f695
kanno-takumi <bp20065@shibaura-it.ac.jp>
2024-02-22 19:17:23 +0900

    cardの大きさ

--- components/card.js
+++ components/card.js
@@ -17,8 +17,8 @@ export default function Card(props){
                 // priority//スタイルの優先順位
                 src={props.image}
                 // className={styles.profilePictures}
-                height={175}//外枠のpaddingが10のため-20にしておく
-                width={175}
+                height={170}//外枠のpaddingが10のため-20にしておく
+                width={170}
                 alt=""
                 // fill
                 // style={{objectFit:'cover',}}
